9d6ba825a07a4340902430e5fdf74eac4e0c65a1
qwertyprob <eqspertars@gmail.com>
2024-10-06 17:15:27 +0300

    Update file Startup/Program into Program from .net8 Minimal API

--- Medcard.Mvc/Program.cs
+++ Medcard.Mvc/Program.cs
@@ -1,26 +1,96 @@
+using Medcard.DbAccessLayer.Entities;
+using Medcard.DbAccessLayer.Interfaces;
+using Medcard.DbAccessLayer.Repositories;
+using Medcard.Mvc.Mapping;
+using Medcard.Mvc.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Medcard.Mvc.Abstractions;
+using Medcard.DbAccessLayer;
 
-namespace MedcardMvc
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Регистрация сервисов
+builder.Services.AddScoped<IMedcardRepository, MedcardRepository>();
+builder.Services.AddScoped<IMedcardServiceMvc, MedcardServiceMvc>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<IAuthServiceMvc, AuthServiceMvc>();
+builder.Services.AddScoped<ISearchRepository, SearchRepository>();
+builder.Services.AddScoped<ISearchServiceMvc, SearchServiceMvc>();
+
+builder.Services.AddSingleton<IHostingServiceMvc, HostingServiceMvc>();
+builder.Services.AddSingleton<IEncrypt, Encrypt>();
+
+builder.Services.AddSession(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Authorization/Auth";
+    });
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAutoMapper(typeof(MappingProfileMvc));
+
+// Получение строки подключения
+var hostingService = builder.Services.BuildServiceProvider().GetService<IHostingServiceMvc>();
+var connectionDb = hostingService.GetEnvironmentVariable();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionDb));
+
+// Добавление поддержки MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Конфигурация pipeline приложения
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Medcard/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Определение маршрутов
+app.MapControllerRoute(
+    name: "medcardUpdateRoute",
+    pattern: "Medcard/Update/{id}",
+    defaults: new { controller = "Medcard", action = "Update" });
+
+app.MapControllerRoute(
+    name: "searchRoute",
+    pattern: "Search/SearchMedcard/{searchItem?}",
+    defaults: new { controller = "Search", action = "SearchMedcard" });
+
+app.MapControllerRoute(
+    name: "medcardDefaultRoute",
+    pattern: "{controller=Medcard}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "authRoute",
+    pattern: "{controller=Authorization}/{action=Auth}");
+
+app.Run();
